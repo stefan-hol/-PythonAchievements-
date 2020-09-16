81d62cb0bcf144bfb7784bd6051065bb381f8f5b
stefan <stefan.hol@hotmail.com>
2020-09-16 14:57:51 +0200

    mijn map met mijn gedicht

---
+++ PYTB1L2PoemPrinting/stefan_gedicht.py
@@ -0,0 +1,37 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+vers1 = "Mijn naam is stefan hol. Maar ik heb geen sokken van schapenwol."
+vers2 = "Ik woon in graft. maar word daarom niet bestraft"
+vers3 = "ik hang graag met mijn vrienden. helaas zijn het niet mijn bedienden"
+vers4 = "ik doe nu de oplijding gamedevelopment. en ik ben daar een student."
+vers5 = "ik speel gitaar in een band. maar we verdienen nog geen ene cent"
+vers6 = "ik kijk how i met your mother met mijn zus. en helaas is ze geen rus"
+SyntaxError: multiple statements found while compiling a single statement
+>>> vers1 = "Mijn naam is stefan hol. Maar ik heb geen sokken van schapenwol."
+vers2 = "Ik woon in graft. maar word daarom niet bestraft"
+vers3 = "ik hang graag met mijn vrienden. helaas zijn het niet mijn bedienden"
+vers4 = "ik doe nu de oplijding gamedevelopment. en ik ben daar een student."
+vers5 = "ik speel gitaar in een band. maar we verdienen nog geen ene cent"
+vers6 = "ik kijk how i met your mother met mijn zus. en helaas is ze geen rus"
+
+SyntaxError: multiple statements found while compiling a single statement
+>>> vers1 = "Mijn naam is stefan hol. Maar ik heb geen sokken van schapenwol."
+
+>>> vers2 = "Ik woon in graft. maar word daarom niet bestraft"
+
+>>> 
+vers3 = "ik hang graag met mijn vrienden. helaas zijn het niet mijn bedienden"
+
+>>> print(vers1)
+Mijn naam is stefan hol. Maar ik heb geen sokken van schapenwol.
+>>> vers4 = "ik doe nu de oplijding gamedevelopment. en ik ben daar een student."
+
+>>> vers5 = "ik speel gitaar in een band. maar we verdienen nog geen ene cent"
+
+>>> 
+vers6 = "ik kijk how i met your mother met mijn zus. en helaas is ze geen rus"
+
+>>> print(vers1 + vers2 + vers3 + vers4 + vers5 + vers6)
+Mijn naam is stefan hol. Maar ik heb geen sokken van schapenwol.Ik woon in graft. maar word daarom niet bestraftik hang graag met mijn vrienden. helaas zijn het niet mijn bediendenik doe nu de oplijding gamedevelopment. en ik ben daar een student.ik speel gitaar in een band. maar we verdienen nog geen ene centik kijk how i met your mother met mijn zus. en helaas is ze geen rus
+>>> 
