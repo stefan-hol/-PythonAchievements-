0b50d53c14d3ee8e16640eef9d54967854a88a96
stefan <stefan.hol@hotmail.com>
2020-09-24 10:27:53 +0200

    mijn eindelijk werkende beetje rommelige hoger lager spelletje

---
+++ hoger_lager.py
@@ -0,0 +1,72 @@
+# hoger lager maken spelletje
+import random
+
+willekeur = random.randrange(1,11)
+nieuw  = random.randrange(1,11) 
+
+	
+print("het getal van 1 / 10 is momenteel ", willekeur)
+print("word het volgende getal hoger [H] of lager [L]?")
+
+antwoord = input()
+
+print(nieuw)
+
+if willekeur < nieuw and antwoord == "L":
+	print("dat is fout")
+	print("game over")	
+	
+elif willekeur > nieuw and antwoord == "H":
+	print("dat is fout")
+	print("game over")
+     
+
+elif willekeur == nieuw:
+	print ("that sucks its the same")
+	print("game over")
+		
+elif (willekeur < nieuw and antwoord == "H"):	
+	print("goedzo")
+	print("kijken of je der nog 1 kan?")
+    
+elif (willekeur > nieuw and antwoord == "L"):
+	print("goedzo")
+	print("kijken of je der nog 1 kan?")
+	
+while((willekeur > nieuw and antwoord == "L")or(willekeur < nieuw and antwoord == "H")):	
+	willekeur = random.randrange(1,11)
+	nieuw  = random.randrange(1,11) 
+	
+	print("het getal van 1 / 10 is momenteel ", willekeur)
+	print("word het volgende getal hoger [H] of lager [L]?")
+
+
+	antwoord = input()
+
+	print(nieuw)
+
+
+	if willekeur < nieuw and antwoord == "L":
+		print("dat is fout")
+		print("game over")	
+	
+	elif willekeur > nieuw and antwoord == "H":
+		print("dat is fout")
+		print("game over")
+     
+
+	elif willekeur == nieuw:
+		print ("that sucks its the same")
+		print("game over")
+		
+	elif (willekeur < nieuw and antwoord == "H"):	
+		print("goedzo")
+		print("kijken of je der nog 1 kan?")
+	
+	elif (willekeur > nieuw and antwoord == "L"):
+		print("goedzo")
+		print("kijken of je der nog 1 kan?")
+		
+		
+		
+		
