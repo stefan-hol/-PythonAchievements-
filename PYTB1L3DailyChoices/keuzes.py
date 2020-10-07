634858e5e689a5d0bfe46040e1630ef7c1f83aa6
stefan <stefan.hol@hotmail.com>
2020-10-07 14:08:45 +0200

    mijn dranken voorkeuren

---
+++ PYTB1L3DailyChoices/keuzes.py
@@ -0,0 +1,50 @@
+print("oke een paar leuke vragen over dranken: koffie of thee? [1]=koffie [2]=thee")
+drank = input()
+
+if(drank == "1"):
+	print("aaah een man van cultuur")
+
+elif(drank == "2"):
+	print("aaah geen man van cultuur dus")
+	
+print("[bier]=1 [wijn]=2 [vodka]=3.")
+drank = input()
+
+if(drank == "1"):
+	print("haha bier")
+
+elif(drank == "2"):
+	print("haha wijnen")
+	
+else:
+	print("haha rocket fuel")
+	
+
+print("[soep]=1 of [bouillon]=2")
+
+drank = input()
+
+if(drank == "1"):
+	print("goeie keuze")
+elif(drank == "2"):
+	print("geen slechte maar soep wint het")
+	
+print("[cola]=1 of [fanta]=2")
+
+drank = input()
+
+if(drank == "1"):
+	print("geen slechte maar fanta wint het")
+elif(drank == "2"):
+	print("jaaa fanta lets go")
+	
+print("nu als laatste [melk]=1 of [water]=2")
+drank = input()
+
+if(drank == "1"):
+	print("foute keuze melk zuigt")
+
+elif(drank == "2"):
+	print("ja goeie keuze fuck melk")
+	
+	
