a5cb699446c32dd55d29da515883f2f3cef89199
stefan <stefan.hol@hotmail.com>
2020-10-21 12:43:23 +0200

    koekjes

---
+++ Python opdrachten/hahahahaha_lijst.py
@@ -0,0 +1,11 @@
+list = ["boterkoek", "rozekoek", "kletskop", 69, float(69.42069)]
+
+print(list)
+list.reverse()
+print(list)
+list.reverse()
+print(list)
+list.remove("kletskop")
+print(list)
+list.insert(2, "speculoos")
+print(list)
